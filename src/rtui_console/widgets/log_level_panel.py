a7e759a1239b792fec669c0ab77e532aacd28798
Taiga Arai <taiga.arai@whill.inc>
2025-08-28 02:03:17 +0900

    modify log level bug

--- src/rtui_console/widgets/log_level_panel.py
+++ src/rtui_console/widgets/log_level_panel.py
@@ -46,6 +46,7 @@ class LogLevelPanel(Static):
         super().__init__(**kwargs)
         self.selected_levels = {"ALL"}
         self.level_checkboxes = {}
+        self._updating_ui = False  # Flag to prevent event loops
 
     def compose(self) -> ComposeResult:
         with ScrollableContainer():
@@ -72,17 +73,25 @@ class LogLevelPanel(Static):
     @on(Checkbox.Changed)
     def on_checkbox_changed(self, event: Checkbox.Changed) -> None:
         """Handle checkbox state changes"""
+        # Skip if we're programmatically updating UI
+        if self._updating_ui:
+            return
+
         checkbox = event.checkbox
 
         if checkbox.id == "level_all":
             # All Levels checkbox toggled
             if checkbox.value:
-                # Select all levels
+                # Select all levels - clear individual selections
                 self.selected_levels = {"ALL"}
+                self._updating_ui = True
                 for level_checkbox in self.level_checkboxes.values():
                     level_checkbox.value = False
+                self._updating_ui = False
             else:
-                self.selected_levels = set()
+                # "All Levels" manually unchecked
+                # Remove "ALL" from selection but keep individual levels
+                self.selected_levels.discard("ALL")
         else:
             # Individual level checkbox toggled
             level_value = getattr(checkbox, 'level_value', None)
@@ -92,10 +101,13 @@ class LogLevelPanel(Static):
             all_checkbox = self.query_one("#level_all", Checkbox)
 
             if checkbox.value:
-                # Level selected
+                # Level selected - remove "ALL" and add specific level
                 self.selected_levels.discard("ALL")
                 self.selected_levels.add(level_value)
+                # Safely update "All Levels" checkbox without triggering events
+                self._updating_ui = True
                 all_checkbox.value = False
+                self._updating_ui = False
             else:
                 # Level deselected
                 self.selected_levels.discard(level_value)
@@ -103,7 +115,9 @@ class LogLevelPanel(Static):
                 # If no levels selected, select "All Levels"
                 if not self.selected_levels:
                     self.selected_levels.add("ALL")
+                    self._updating_ui = True
                     all_checkbox.value = True
+                    self._updating_ui = False
 
         # Send updated selection
         self.post_message(LevelFilterChanged(list(self.selected_levels)))
